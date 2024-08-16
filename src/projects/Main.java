f03cbebbab61e9efcdb28d0f9d26dd92851b1510
Amankumar5543 <ashrivashtava@gmail.com>
2024-08-16 10:09:43 +0530

    new changes

--- src/projects/Main.java
+++ src/projects/Main.java
@@ -6,10 +6,15 @@ public class Main {
         Banking Sahil = new Banking("Sahil kumar",10002222,50000.34,true,"abc");
         Banking Mukesh= new Banking("Mukesh kumar",10003333,6765.32,false,"xyz");
         Banking Mrinal = new Banking("Mrinal kumar",10004444,100000.43,true,"xyz");
+        System.out.println(("Welcome To the bank of India ").toUpperCase());
+        Sahil.accountDetails();
+
+        Sahil.closeAccount();
+        Sahil.accountDetails();
+        Sahil.withdrawMoney(500);
 
 
 
 
-      
     }
 }
